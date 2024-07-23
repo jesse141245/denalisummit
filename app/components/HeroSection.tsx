8cc5e8aaab0b4763f4e508286c8c712202c5881a
jesse141245 <126207272+jesse141245@users.noreply.github.com>
2024-07-22 17:49:31 -0700

    Update HeroSection.tsx

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link';
 const HeroSection: React.FC = () => {
   return (
     <div className='hero-container'>
-      <video autoPlay muted loop>
+      <video autoPlay muted loop disablePictureInPicture>
         <source src="/mountainvideo.mp4" type="video/mp4" />
       </video>
       <h1>We buy simple businesses.</h1>
